6e2f612d62808f48d76ba72cf060d289f89b9f1c
carol <carolina.df@outlook.com>
2023-04-02 12:10:00 -0300

    Changes on blog

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "A ARTE MARCIAL MAIS ANTIGA DO MUNDO",
+    "description": "Veja qual é a arte marcial mais antiga da humanidade",
+    "photoCover":"https://s2.glbimg.com/g-fvu_lzyB6W6VlpDGBwMg1aPYY=/e.glbimg.com/og/ed/f/original/2020/01/17/bruce_lee.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
+    "title": "Luta brasileira Marajoara",
     "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "photoCover":"https://revistaproativa.com.br/wp-content/uploads/2021/01/Luta-marajoara-uma-luta-genuinamente-brasileira-1.jpg"
   }
 ]
